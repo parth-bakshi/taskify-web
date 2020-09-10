0a46e05fbd32e38ecb2262d751ac72828e2adfc6
swapnilpatil94 <swapnilp576p@gmail.com>
2020-09-10 11:51:39 +0530

    Add todo DAte time field

--- src/api_services/urls.js
+++ src/api_services/urls.js
@@ -2,11 +2,11 @@ const baseURL = "http://localhost:8000";
 const devUrl = "https://taskify-api.herokuapp.com"
 
 export const apiURLs = {
-  login: () => `${devUrl}/user/login`,
-  getUser: () => `${devUrl}/user`,
-  getTasks: () => `${devUrl}/task/get-all-tasks`,
-  toggleTask: () => `${devUrl}/task/toggle-task-status`,
-  deleteTask: () => `${devUrl}/task/delete-task`,
-  createTask: () => `${devUrl}/task/create`,
-  createCategory: () => `${devUrl}/user/create/category`,
+  login: () => `${baseURL}/user/login`,
+  getUser: () => `${baseURL}/user`,
+  getTasks: () => `${baseURL}/task/get-all-tasks`,
+  toggleTask: () => `${baseURL}/task/toggle-task-status`,
+  deleteTask: () => `${baseURL}/task/delete-task`,
+  createTask: () => `${baseURL}/task/create`,
+  createCategory: () => `${baseURL}/user/create/category`,
 };
