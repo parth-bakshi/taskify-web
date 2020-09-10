1d685c83121f0f4ae851cc29a0a0c6de2d403f7a
prateek-garg-mnr <prateek.garg@mckinleyrice.com>
2020-09-10 18:24:06 +0530

    notification trial 1

--- src/App.js
+++ src/App.js
@@ -56,29 +56,31 @@ function App() {
       .then((response) => {
         if (response.data.data) {
           response.data.data.map((task) => {
-            // const taskDate = moment(task.date, "DD-MM-YYYY HH:mm");
-            // console.log((new Date().getTime() - 60000 < taskDate: "date"));
-            // if (
-            //   taskDate.date() === moment().date() &&
-            //   taskDate.month() === moment().month() &&
-            //   taskDate.year() === moment().year() &&
-            //   taskDate.hour() === moment().hour() &&
-            //   taskDate.minute() === moment().minute()
-            // ) {
-            //   Push.create(task.name, {
-            //     body: task.description,
-            //     icon: "/icon.png",
-            //     timeout: 4000,
-            //     onClick: function () {
-            //       window.focus();
-            //       this.close();
-            //     },
-            //   });
-            // }
+            const taskDate = moment(task.date, "DD-MM-YYYY HH:mm");
+            console.log(taskDate.minute() - moment().minute() === 1);
+            if (
+              taskDate.date() === moment().date() &&
+              taskDate.month() === moment().month() &&
+              taskDate.year() === moment().year() &&
+              taskDate.hour() === moment().hour() &&
+              taskDate.minute() - moment().minute() === 1 &&
+              localStorage.getItem("id") !== task._id
+            ) {
+              localStorage.setItem("id", task._id);
+              Push.create(task.name, {
+                body: task.description,
+                icon: "/icon.png",
+                timeout: 6000,
+                onClick: function () {
+                  window.focus();
+                  this.close();
+                },
+              });
+            }
           });
         }
       });
-  }, 1000);
+  }, 30000);
 
   return (
     <Router>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNE_0Vq9CCpVb3LLox2ciFND_w-0yjVKM",
+  authDomain: "snappy-bucksaw-289000.firebaseapp.com",
+  databaseURL: "https://snappy-bucksaw-289000.firebaseio.com",
+  projectId: "snappy-bucksaw-289000",
+  storageBucket: "snappy-bucksaw-289000.appspot.com",
+  messagingSenderId: "254081515281",
+  appId: "1:254081515281:web:c3be4052df2b275eba16b0",
+  measurementId: "G-CH8WB8CB4M",
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
